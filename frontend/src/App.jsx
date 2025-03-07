bc4d86ebdb03e08c07317a3198b94cc140855dca
Ayushkumarcoder <ayushrajeshmishra20@gmail.com>
2025-03-07 18:13:33 +0530

    completed the navigation bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Collection from './pages/Collection'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import Product from './pages/Product'
+import Orders from './pages/Orders'
+import PlaceOrder from './pages/PlaceOrder'
+import Navbar from './components/Navbar'
 
 const App = () => {
+    
+
+
   return (
-    <div className='px-4 sm:px'>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+
+      <Navbar />
+
+
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/collection' element={<Collection/>}/>
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/product/:productId' element={<Product></Product>}></Route>
+        <Route path='/cart' element= {<Cart></Cart>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/place-order' element={<PlaceOrder></PlaceOrder>}></Route>
+        <Route path='/orders' element = {<Orders></Orders>}></Route>
+
+
+
+      </Routes>
 
     </div>
   )
