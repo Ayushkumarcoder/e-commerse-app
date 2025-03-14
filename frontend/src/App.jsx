2679f81dbc2c6aa7420dd5f4ba61d9cd403be929
Ayushkumarcoder <ayushrajeshmishra20@gmail.com>
2025-03-14 17:52:01 +0530

    created the cart page made the item couting in cart responsive created total amount section in card creatd PlaceOrder page created orders page as well

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,9 @@ import PlaceOrder from './pages/PlaceOrder'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar.jsx'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+
 
 const App = () => {
     
@@ -19,6 +22,9 @@ const App = () => {
 
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <ToastContainer>
+        
+      </ToastContainer>
 
       <Navbar />
       <SearchBar></SearchBar>
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,7 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 //creating context api 
 
@@ -13,9 +15,91 @@ const ShopContextProvider = (prop) =>{
     //for search 
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
+    const navigate = useNavigate();
+
+    //for cart 
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = async(itemId, size) =>{
+
+        if(!size){
+            toast.error('Select Product Size');
+            return;
+        }
+
+        setCartItems(prevCartItems => {
+            let cartData = { ...prevCartItems };
+    
+            if (cartData[itemId]) {
+                if (cartData[itemId][size]) {
+                    cartData[itemId][size] += 1;
+                } else {
+                    cartData[itemId][size] = 1;
+                }
+            } else {
+                cartData[itemId] = { [size]: 1 };
+            }
+            
+            return cartData;
+        });
+    }
+
+    const getCartCount = () =>{
+        let totalCount = 0;
+        for(const items in cartItems){
+            for(const item in cartItems[items]){
+                try {
+                    if(cartItems[items][item]){
+                        totalCount += cartItems[items][item];
+                    }
+                } catch (error) {
+                    
+                }
+            }
+        }
+        return totalCount;
+    }
+
+    useEffect(()=>{
+        console.log(cartItems);
+    },[cartItems])
+
+
+    const updateQuantity = async(itemId, size, quantity) =>{
+        let cartData = structuredClone(cartItems);
+
+        cartData[itemId][size] = quantity;
+        setCartItems(cartData);
+    }
+
+    const getCartAmount = () =>{
+        let totalAmount = 0;
+        for(const items in cartItems){
+            let itemInfo = products.find((products) => products._id === items);
+
+            for(const item in cartItems[items]){
+                try {
+                    if(cartItems[items][item] > 0){
+                        totalAmount += itemInfo.price * cartItems[items][item];
+                    }
+                } catch (error) {
+                    
+                }
+            }
+        }
+        return totalAmount;
+    }
+
+
+
 
     const value = {                  //this is the atom with the value which is being passed
-        products , currency, delivery_fee, search, setSearch, showSearch, setShowSearch
+        products , currency, delivery_fee, search, setSearch, showSearch, setShowSearch,
+        cartItems, addToCart, 
+        getCartCount,
+        updateQuantity,
+        getCartAmount,
+        navigate,
     }
 
     return (
